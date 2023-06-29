e4b8c9c4862fa9db17d7632055e451338eebf067
yangbaechu <abab_1212@naver.com>
2023-06-29 04:48:29 +0000

    Clustering by public data

---
+++ data_utils.py
@@ -0,0 +1,134 @@
+import numpy as np
+from torch.utils.data import Subset
+
+
+def split_noniid(train_idcs, train_labels, alpha, n_clients):
+    """
+    Splits a list of data indices with corresponding labels
+    into subsets according to a dirichlet distribution with parameter
+    alpha
+    """
+    n_classes = train_labels.max() + 1
+    label_distribution = np.random.dirichlet([alpha] * n_clients, n_classes)
+
+    class_idcs = [
+        np.argwhere(train_labels[train_idcs] == y).flatten() for y in range(n_classes)
+    ]
+
+    client_idcs = [[] for _ in range(n_clients)]
+    for c, fracs in zip(class_idcs, label_distribution):
+        for i, idcs in enumerate(
+            np.split(c, (np.cumsum(fracs)[:-1] * len(c)).astype(int))
+        ):
+            client_idcs[i] += [idcs]
+
+    client_idcs = [train_idcs[np.concatenate(idcs)] for idcs in client_idcs]
+
+    return client_idcs
+
+
+def split_contain_every_class(train_idcs, train_labels, n_clients, data_per_class, Imbalance_ratio):
+    """
+    Splits a list of data indices with corresponding labels
+    into subsets according to deterministic rule
+    """
+
+    data_per_class = data_per_class
+    n_cluster = 3
+    client_per_cluster = 3
+    n_class = 20
+    Imbalance_ratio = Imbalance_ratio
+    client_idcs = [[] for _ in range(n_clients)]
+
+    class_idcs = [
+        np.where(train_labels == c)[0].tolist()[:4000] for c in range(n_class)
+    ]
+
+    for c in range(n_cluster):
+        for client in range(client_per_cluster):
+            # split indices for class c across clients for that class
+            idx = []
+            for class_num, class_index in enumerate(class_idcs):
+                start = (n_cluster * 3 + n_clients) * data_per_class
+                end = start + data_per_class
+                # Allocate 2 times more data of class 0, 1, 2 to cluster 1 and so on
+                if class_num // 3 == c:
+                    end += int(data_per_class * (Imbalance_ratio - 1))
+                idx.extend(class_index[start:end])
+
+            client_idcs[client + c * client_per_cluster] += idx
+
+    # Convert to numpy array
+    client_idcs = [np.array(idcs) for idcs in client_idcs]
+
+    return client_idcs
+
+
+def split_not_contain_every_class(train_idcs, train_labels, n_clients):
+    """
+    Splits a list of data indices with corresponding labels
+    into subsets according to deterministic rule
+    """
+
+    n_classes = 3  # we have three classes 0, 1 and 2
+    client_idcs = [[] for _ in range(n_clients)]
+    clients_per_class = (
+        n_clients // n_classes
+    )  # assume n_clients is a multiple of n_classes
+
+    for c in range(n_classes):
+        # find indices of class c
+        class_idcs_1 = np.where(train_labels == c)[0].tolist()[:4000]
+        class_idcs_2 = np.where(train_labels == c + 3)[0].tolist()[:4000]
+
+        # split indices for class c across clients for that class
+        for i in range(clients_per_class):
+            start = i * 200
+            end = start + 200
+
+            client_idcs[i * n_classes + c] += list(class_idcs_1[start:end])
+            client_idcs[i * n_classes + c] += list(class_idcs_2[start:end])
+
+    # Convert to numpy array
+    client_idcs = [np.array(idcs) for idcs in client_idcs]
+
+    return client_idcs
+
+
+def generate_server_idcs(test_idcs, test_labels, n_class=10):
+    """
+    Generate server indices with identical label distribution as client_idcs
+    """
+
+    server_idcs = []
+    data_per_class = 10
+
+    class_idcs = [np.where(test_labels == c)[0].tolist() for c in range(n_class)]
+
+    for class_num, class_index in enumerate(class_idcs):
+        start = 0
+        end = start + data_per_class
+        server_idcs.extend(class_index[start:end])
+
+    # Convert to numpy array
+    server_idcs = np.array(server_idcs)
+    server_idcs += 10000
+
+    return server_idcs
+
+
+
+class CustomSubset(Subset):
+    """A custom subset class with customizable data transformation"""
+
+    def __init__(self, dataset, indices, subset_transform=None):
+        super().__init__(dataset, indices)
+        self.subset_transform = subset_transform
+
+    def __getitem__(self, idx):
+        x, y = self.dataset[self.indices[idx]]
+
+        if self.subset_transform:
+            x = self.subset_transform(x)
+
+        return x, y
---
+++ fl_devices.py
@@ -0,0 +1,288 @@
+import random
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def train_op(model, loader, optimizer, epochs=1):
+    model.train()
+    for ep in range(epochs):
+        running_loss, samples = 0.0, 0
+        for x, y in loader:
+            x, y = x.to(device), y.to(device)
+
+            optimizer.zero_grad()
+
+            loss = torch.nn.CrossEntropyLoss()(model(x), y)
+            running_loss += loss.item() * y.shape[0]
+            samples += y.shape[0]
+
+            loss.backward()
+            optimizer.step()
+        # print(f"running_loss: {running_loss}")
+        # print(f"samples:{samples}")
+
+    return running_loss / samples
+
+
+def eval_op(model, loader):
+    model.train()
+    samples, correct = 0, 0
+
+    with torch.no_grad():
+        for i, (x, y) in enumerate(loader):
+            x, y = x.to(device), y.to(device)
+
+            y_ = model(x)
+            _, predicted = torch.max(y_.data, 1)
+
+            samples += y.shape[0]
+            correct += (predicted == y).sum().item()
+
+    return correct / samples
+
+
+import torch.nn.functional as F
+
+
+def eval_for_server(model, loader):
+    model.eval()
+    samples, correct = 0, 0
+    from collections import defaultdict
+
+    label_correct = defaultdict(int)  # Counts of correct predictions for each label
+    label_samples = defaultdict(int)  # Counts of total samples for each label
+    label_predicted = defaultdict(int)  # Counts of predicted labels
+    label_soft_sum = defaultdict(float)  # Sum of soft outputs per label
+
+    with torch.no_grad():
+        for i, (x, y) in enumerate(loader):
+            x, y = x.to(device), y.to(device)
+
+            y_ = model(x)
+            soft_output = F.softmax(y_, dim=1)
+            _, predicted = torch.max(y_.data, 1)
+
+            for label in y.tolist():
+                label_samples[label] += 1
+
+            for label in predicted.tolist():
+                label_predicted[label] += 1
+
+            for i, label in enumerate(y.tolist()):
+                label_soft_sum[label] += soft_output[i][label].item()
+
+            correct_preds = predicted == y
+            for label, correct in zip(y.tolist(), correct_preds.tolist()):
+                if correct:
+                    label_correct[label] += 1
+
+    label_accuracies = {
+        label: label_correct.get(label, 0) / label_samples[label]
+        for label in label_samples
+    }
+
+    label_diff = {
+        label: label_predicted[label] - label_samples[label] for label in label_samples
+    }
+
+    return label_accuracies, label_predicted, label_soft_sum, label_diff
+
+
+def copy(target, source):
+    for name in target:
+        target[name].data = source[name].data.clone()
+
+
+def get_dW(target, minuend, subtrahend):
+    for name in target:
+        target[name].data += minuend[name].data.clone() - subtrahend[name].data.clone()
+
+
+def reduce_add_average(targets, sources):
+    for target in targets:
+        for name in target:
+            tmp = torch.mean(
+                torch.stack([source[name].data for source in sources]), dim=0
+            ).clone()
+            target[name].data += tmp
+
+
+def flatten(source):
+    return torch.cat([value.flatten() for value in source.values()])
+
+
+def pairwise_angles(sources):
+    angles = torch.zeros([len(sources), len(sources)])
+    for i, source1 in enumerate(sources):
+        for j, source2 in enumerate(sources):
+            s1 = flatten(source1)
+            s2 = flatten(source2)
+            angles[i, j] = torch.sum(s1 * s2) / (
+                torch.norm(s1) * torch.norm(s2) + 1e-12
+            )
+
+    return angles.numpy()
+
+
+class FederatedTrainingDevice(object):
+    def __init__(self, model_fn, data):
+        self.model = model_fn().to(device)
+        self.data = data
+        self.W = {key: value for key, value in self.model.named_parameters()}
+
+    def evaluate(self, loader=None):
+        return eval_op(self.model, self.eval_loader if not loader else loader)
+
+
+class Client(FederatedTrainingDevice):
+    def __init__(
+        self, model_fn, optimizer_fn, data, idnum, batch_size=128, train_frac=0.7
+    ):
+        super().__init__(model_fn, data)
+        self.optimizer = optimizer_fn(self.model.parameters())
+
+        self.data = data
+        n_train = int(len(data) * train_frac)
+        n_eval = len(data) - n_train
+
+        data_train, data_eval = torch.utils.data.random_split(
+            self.data, [n_train, n_eval]
+        )
+
+        self.train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
+        self.eval_loader = DataLoader(data_eval, batch_size=batch_size, shuffle=False)
+
+        self.id = idnum
+
+        self.dW = {
+            key: torch.zeros_like(value) for key, value in self.model.named_parameters()
+        }
+        self.W_old = {
+            key: torch.zeros_like(value) for key, value in self.model.named_parameters()
+        }
+
+    def synchronize_with_server(self, server):
+        copy(target=self.W, source=server.W)
+        self.W_original = self.W
+
+    def compute_weight_update(self, epochs=1, loader=None):
+        copy(target=self.W_old, source=self.W)
+        self.optimizer.param_groups[0]["lr"] *= 0.99
+        train_stats = train_op(
+            self.model,
+            self.train_loader if not loader else loader,
+            self.optimizer,
+            epochs,
+        )
+        get_dW(target=self.dW, minuend=self.W, subtrahend=self.W_old)
+        return train_stats
+
+    def reset(self):
+        copy(target=self.W, source=self.W_original)
+
+
+class Server(FederatedTrainingDevice):
+    def __init__(self, model_fn, data):
+        super().__init__(model_fn, data)
+        self.loader = DataLoader(self.data, batch_size=128, shuffle=False)
+        self.model_cache = []
+
+    def evaluate(self, model):
+        (
+            label_accuracies,
+            label_predicted,
+            label_soft_sum,
+            label_diff,
+        ) = eval_for_server(model, self.loader)
+
+        return (
+            label_accuracies,
+            label_predicted,
+            label_soft_sum,
+            label_diff,
+        )
+
+    def select_clients(self, clients, frac=1.0):
+        return random.sample(clients, int(len(clients) * frac))
+
+    def aggregate_weight_updates(self, clients):
+        reduce_add_average(target=self.W, sources=[client.dW for client in clients])
+
+    def compute_pairwise_similarities(self, clients):
+        return pairwise_angles([client.dW for client in clients])
+
+    def cluster_clients(self, S):
+        clustering = AgglomerativeClustering(
+            affinity="precomputed", linkage="complete"
+        ).fit(-S)
+
+        c1 = np.argwhere(clustering.labels_ == 0).flatten()
+        c2 = np.argwhere(clustering.labels_ == 1).flatten()
+        return c1, c2
+
+    def cluster_clients_DBSCAN(self, S):
+        clustering = DBSCAN(eps=1.5, min_samples=1).fit(-S)  # eps 0.8 ~ 1로 test
+        return clustering.labels_
+
+    def cluster_clients_GMM(self, S):
+        gmm = GaussianMixture(n_components=3)
+        gmm.fit(S)
+        labels = np.argmax(gmm.predict_proba(S), axis=1)
+        c1 = np.argwhere(labels == 0).flatten()
+        c2 = np.argwhere(labels == 1).flatten()
+        c3 = np.argwhere(labels == 1).flatten()
+        return c1, c2, c3
+
+    def cluster_clients_BGM(self, S):
+        bgm = BayesianGaussianMixture(n_components=2)
+        bgm.fit(S)
+        labels = np.argmax(bgm.predict_proba(S), axis=1)
+        c1 = np.argwhere(labels.labels_ == 0).flatten()
+        c2 = np.argwhere(labels.labels_ == 1).flatten()
+        return c1, c2
+
+    def aggregate_clusterwise(self, client_clusters):
+        for cluster in client_clusters:
+            reduce_add_average(
+                targets=[client.W for client in cluster],
+                sources=[client.dW for client in cluster],
+            )
+        return
+
+    def get_average_dw(self, clients):
+        # Initialize an empty dictionary to store the average dW
+        avg_dW = {}
+
+        # Iterate through each tensor name in the first client's dW to add up dW from all clients
+        for name in clients[0].dW:
+            avg_dW[name] = sum(client.dW[name].data for client in clients)
+
+        # Divide the summed dW by the number of clients to compute the average
+        for name in avg_dW:
+            avg_dW[name] /= len(clients)
+
+        return avg_dW
+
+    def compute_max_update_norm(self, cluster):
+        return np.max([torch.norm(flatten(client.dW)).item() for client in cluster])
+
+    def compute_mean_update_norm(self, cluster):
+        return torch.norm(
+            torch.mean(torch.stack([flatten(client.dW) for client in cluster]), dim=0)
+        ).item()
+
+    def cache_model(self, idcs, params, accuracies):
+        self.model_cache += [
+            (
+                idcs,
+                {name: params[name].data.clone() for name in params},
+                [accuracies[i] for i in idcs],
+            )
+        ]
