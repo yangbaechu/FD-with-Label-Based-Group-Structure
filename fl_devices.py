a983b494f9e9e36f8c4310ddd831420dc3b8fe64
yangbaechu <abab_1212@naver.com>
2023-07-04 14:28:14 +0000

    change to use CIFAR10

--- fl_devices.py
+++ fl_devices.py
@@ -65,6 +65,16 @@ def eval_op(model, loader):
 
     return correct / samples
 
+def refine_as_not_true(logits, targets, num_classes):
+    nt_positions = torch.arange(0, num_classes).to(logits.device)
+    nt_positions = nt_positions.repeat(logits.size(0), 1)
+    nt_positions = nt_positions[nt_positions[:, :] != targets.view(-1, 1)]
+    nt_positions = nt_positions.view(-1, num_classes - 1)
+
+    logits = torch.gather(logits, 1, nt_positions)
+
+    return logits
+
 class DistillationLoss(nn.Module):
     def __init__(self, alpha=0.5, T=2.0):
         super(DistillationLoss, self).__init__()
@@ -80,6 +90,41 @@ class DistillationLoss(nn.Module):
         )
         return hard_loss + soft_loss
     
+class NTD_Loss(nn.Module):
+    """Not-true Distillation Loss"""
+
+    def __init__(self, num_classes=10, tau=3, beta=1):
+        super(NTD_Loss, self).__init__()
+        self.CE = nn.CrossEntropyLoss()
+        self.MSE = nn.MSELoss()
+        self.KLDiv = nn.KLDivLoss(reduction="batchmean")
+        self.num_classes = num_classes
+        self.tau = tau
+        self.beta = beta
+
+    def forward(self, logits, targets, dg_logits):
+        ce_loss = self.CE(logits, targets)
+        ntd_loss = self._ntd_loss(logits, dg_logits, targets)
+
+        loss = ce_loss + self.beta * ntd_loss
+
+        return loss
+
+    def _ntd_loss(self, logits, dg_logits, targets):
+        """Not-tue Distillation Loss"""
+
+        # Get smoothed local model prediction
+        logits = refine_as_not_true(logits, targets, self.num_classes)
+        pred_probs = F.log_softmax(logits / self.tau, dim=1)
+
+        # Get smoothed global model prediction
+        with torch.no_grad():
+            dg_logits = refine_as_not_true(dg_logits, targets, self.num_classes)
+            dg_probs = torch.softmax(dg_logits / self.tau, dim=1)
+
+        loss = (self.tau ** 2) * self.KLDiv(pred_probs, dg_probs)
+
+        return loss
 
 def eval_for_server(model, loader):
     model.eval()
@@ -196,7 +241,7 @@ class Client(FederatedTrainingDevice):
         n_train = int(len(data) * train_frac)
         n_eval = len(data) - n_train
         
-        print(f'data length: {len(data)}')
+        #print(f'data length: {len(data)}')
         
         data_train, data_eval = torch.utils.data.random_split(self.data, [n_train, n_eval])
         
@@ -340,7 +385,29 @@ class Server(FederatedTrainingDevice):
             label_soft_sum,
             label_diff,
         )
+    
+    
+    def evaluate_distil(self, model):
+        model.eval()  # Set model to evaluation mode
+        samples, correct = 0, 0
 
+        with torch.no_grad():
+            for i, (x, y) in enumerate(self.loader):
+                # Evaluate only on 10% of the data
+                if i > len(self.loader) // 10:
+                    break
+
+                x, y = x.to(device), y.to(device)
+
+                y_ = model(x)
+                _, predicted = torch.max(y_.data, 1)
+
+                samples += y.shape[0]
+                correct += (predicted == y).sum().item()
+
+        return correct / samples if samples > 0 else 0
+
+    
     def select_clients(self, clients, frac=1.0):
         return random.sample(clients, int(len(clients) * frac))
 
